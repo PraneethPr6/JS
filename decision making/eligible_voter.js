b2201fcd3e12be90a0c8c8d3df9c6fc3f3fc65be
praneeth <107698333+PraneethPr6@users.noreply.github.com>
2022-08-02 13:15:50 +0530

    Add files via upload

---
+++ decision making/eligible_voter.js
@@ -0,0 +1,15 @@
+
+const isEligible = (a) => {
+    var res;
+    
+    if(a>=18) 
+    {
+      res ="Eligible for Voting";
+      return res;
+    }
+    else
+     {
+       res ="Not Eligible for Voting";
+       return res;
+     }
+};
